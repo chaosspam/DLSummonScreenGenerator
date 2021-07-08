d4e530e93b5c051f89d13115d236c04726ea05fe
chaosspam <84607454+chaosspam@users.noreply.github.com>
2021-07-08 14:08:45 -0700

    Cache some image elements to reduce load time

--- main.js
+++ main.js
@@ -5,6 +5,8 @@
   const textYPos = 1345;
   const textAngle = -8;
   const textHeight = 40;
+  const textures = {};
+  const elementTextSpacing = 5;
 
   window.addEventListener("load", init);
 
@@ -13,15 +15,23 @@
     document.fonts.load("60px dragalialost").then(drawImage).then(setupListener);
   }
 
+  /**
+   * Set up event listeners
+   */
   function setupListener() {
+    // Update image after upload
+    id("portraitUpload").addEventListener("change", changeImage);
+    id("modelUpload").addEventListener("change", changeImage);
+
+    // Draw image after parameter change
     id("name").addEventListener("change", drawImage);
     id("element").addEventListener("change", drawImage);
     id("sparkAmount").addEventListener("change", drawImage);
-    id("portraitUpload").addEventListener("change", changeImage);
-    id("modelUpload").addEventListener("change", changeImage);
     id("portrait").addEventListener("load", drawImage);
     id("model").addEventListener("load", drawImage);
+
     document.querySelectorAll("input[type=number]").forEach(e => {
+      // Sync number input with slider
       qs(`[data-slider="${e.id}"]`).addEventListener("input", sliderUpdateNumInput);
       qs(`[data-slider="${e.id}"]`).addEventListener("change", sliderChangeNumInput);
       e.addEventListener("input", numInputUpdateSlider);
@@ -48,59 +58,91 @@
     id(this.dataset.image).src = window.URL.createObjectURL(this.files[0]);
   }
 
+  async function loadTextures() {
+    if(!textures.background) {
+      textures.background = await loadImage("images/bg.png");
+      textures.fire = await loadImage(`images/fire.png`);
+      textures.wind = await loadImage(`images/wind.png`);
+      textures.water = await loadImage(`images/water.png`);
+      textures.light = await loadImage(`images/light.png`);
+      textures.shadow = await loadImage(`images/shadow.png`);
+      textures.spark = await loadImage("images/spark.png");
+      textures.bar = await loadImage("images/bar.png");
+    }
+  }
+
+  /**
+   * Draws the summon screen based on inputs
+   */
   async function drawImage() {
+    // Get canvas context
     const canvas = id("editor");
     const ctx = canvas.getContext("2d");
-    const img = await loadImage("images/bg.png");
-    const elementImg = await loadImage(`images/${id("element").value}.png`);
+
+    // Load images for use
+    await loadTextures();
+    const background = textures.background;
+    const element = textures[id("element").value];
+    const spark = textures.spark;
+    const bar = textures.bar;
+    const sparkAmount = parseInt(id("sparkAmount").value);
+
+    // Get parameters
     const adventurerName = id("name").value;
-    const spark = await loadImage("images/spark.png");
-    const bar = await loadImage("images/bar.png");
     const portrait = id("portrait");
     const model = id("model");
-    ctx.drawImage(img, 0, 0);
 
-    drawImageOffsetScale(ctx, portrait, id("portraitScale").value, canvas.width / 2, canvas.height / 2, id("portraitOffsetX").value, id("portraitOffsetY").value);
-    drawImageOffsetScale(ctx, model, id("modelScale").value, modelPosX, modelPosY, id("modelOffsetX").value, id("modelOffsetY").value);
+    // Draw background elements
+    ctx.drawImage(background, 0, 0);
+
+    drawImageOffsetScale(ctx, portrait, id("portraitScale").value,
+      canvas.width / 2, canvas.height / 2,
+      id("portraitOffsetX").value, id("portraitOffsetY").value);
+
+    drawImageOffsetScale(ctx, model, id("modelScale").value,
+      modelPosX, modelPosY,
+      id("modelOffsetX").value, id("modelOffsetY").value);
 
     ctx.drawImage(bar, 0, 0);
 
+    // Calculate text position based on text width
+    let textWidth = ctx.measureText(adventurerName).width;
+    let canvasCenter = canvas.width / 2;
+    let elementXPos = canvasCenter - (textWidth + element.width + elementTextSpacing) / 2;
+    let elementYPos = textYPos - textHeight - (element.height - textHeight) / 2;
+    let textXPos = elementXPos + element.width + elementTextSpacing;
+
+    // Rotate the context to draw rotated text
+    ctx.translate(canvasCenter, textYPos);
+    ctx.rotate(textAngle * Math.PI / 180);
+    ctx.translate(-canvasCenter, -textYPos);
+
+    // Draw the shadow of text -> text -> element
     ctx.font = "60px dragalialost";
     ctx.fillStyle = "#6a551f";
-
-
-    let textWidth = ctx.measureText(adventurerName).width;
-    let rotateCenter = canvas.width / 2;
-    let padding = 10;
-    let textStart = rotateCenter - (textWidth + elementImg.width + padding) / 2 + elementImg.width + padding;
-
-    ctx.translate(rotateCenter, textYPos);
-    ctx.rotate(textAngle * Math.PI / 180);
-    ctx.translate(-rotateCenter, -textYPos);
-
-    ctx.fillText(adventurerName, textStart + 5, textYPos + 5);
-
+    ctx.fillText(adventurerName, textXPos + 5, textYPos + 5);
     ctx.fillStyle = "white";
+    ctx.fillText(adventurerName, textXPos, textYPos);
+    ctx.drawImage(element, elementXPos, elementYPos);
 
-    ctx.fillText(adventurerName, textStart, textYPos);
-
-    ctx.drawImage(elementImg, rotateCenter - (textWidth + elementImg.width + padding) / 2, textYPos - textHeight - (elementImg.height - textHeight) / 2);
-
-    ctx.translate(rotateCenter, textYPos);
+    // Rotate the context back to original rotation
+    ctx.translate(canvasCenter, textYPos);
     ctx.rotate(-textAngle * Math.PI / 180);
-    ctx.translate(-rotateCenter, -textYPos);
+    ctx.translate(-canvasCenter, -textYPos);
 
-    for(let i = 0; i < parseInt(id("sparkAmount").value); i++)
+    // Draw sparks
+    for(let i = 0; i < sparkAmount; i++)
     {
-      if(Math.random() > 0.6) {
+      if(Math.random() > 0.75) {
         let sparkScale = Math.min(Math.random(), .8);
         ctx.drawImage(spark, Math.random() * canvas.width,  Math.random() * canvas.height, spark.height * sparkScale, spark.width * sparkScale);
       }
     }
 
+    // Generate download url
     id("download").href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
 
-
+    // Draw the editor canvas on the smaller preview canvas
     const previewCanvas = id("preview");
     const previewCtx = previewCanvas.getContext("2d");
     previewCtx.drawImage(canvas, 0, 0, previewCanvas.width, previewCanvas.height);
