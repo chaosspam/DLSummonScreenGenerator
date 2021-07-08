d6d1282651daef64eff9140ab06cc8e3784325be
chaosspam <84607454+chaosspam@users.noreply.github.com>
2021-07-08 13:01:37 -0700

    Double quotes are in

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 (function () {
 
-  const canvasWidth = 750;
-  const canvasHeight = 1575;
   const modelPosX = 130;
   const modelPosY = 1050;
   const textYPos = 1345;
   const textAngle = -8;
   const textHeight = 40;
 
-  window.addEventListener('load', init);
+  window.addEventListener("load", init);
 
   function init() {
-    document.fonts.load('60px dragalialost').then(drawImage).then(setupListener);
+    document.fonts.load("60px dragalialost").then(drawImage).then(setupListener);
   }
 
   function setupListener() {
-    id('name').addEventListener('change', drawImage);
-    id('element').addEventListener('change', drawImage);
-    id('portraitUpload').addEventListener('change', changeImage);
-    id('modelUpload').addEventListener('change', changeImage);
-    id('portrait').addEventListener('load', drawImage);
-    id('model').addEventListener('load', drawImage);
-    document.querySelectorAll('input[type=number]').forEach(e=>e.addEventListener('change', drawImage))
+    id("name").addEventListener("change", drawImage);
+    id("element").addEventListener("change", drawImage);
+    id("portraitUpload").addEventListener("change", changeImage);
+    id("modelUpload").addEventListener("change", changeImage);
+    id("portrait").addEventListener("load", drawImage);
+    id("model").addEventListener("load", drawImage);
+    document.querySelectorAll("input[type=number]").forEach(e=>e.addEventListener("change", drawImage))
   }
 
   function changeImage() {
@@ -29,29 +27,29 @@
   }
 
   async function drawImage() {
-    const canvas = id('editor');
-    const ctx = canvas.getContext('2d');
-    const img = await loadImage('images/bg.png');
-    const elementImg = await loadImage(`images/${id('element').value}.png`);
-    const adventurerName = id('name').value;
-    const spark = await loadImage('images/spark.png');
-    const bar = await loadImage('images/bar.png');
-    const portrait = id('portrait');
-    const model = id('model');
+    const canvas = id("editor");
+    const ctx = canvas.getContext("2d");
+    const img = await loadImage("images/bg.png");
+    const elementImg = await loadImage(`images/${id("element").value}.png`);
+    const adventurerName = id("name").value;
+    const spark = await loadImage("images/spark.png");
+    const bar = await loadImage("images/bar.png");
+    const portrait = id("portrait");
+    const model = id("model");
     ctx.drawImage(img, 0, 0);
 
-    drawImageOffsetScale(ctx, portrait, id('portraitScale').value, canvasWidth / 2, canvasHeight / 2, id('portraitOffsetX').value, id('portraitOffsetY').value);
-    drawImageOffsetScale(ctx, model, id('modelScale').value, modelPosX, modelPosY, id('modelOffsetX').value, id('modelOffsetY').value);
+    drawImageOffsetScale(ctx, portrait, id("portraitScale").value, canvas.width / 2, canvas.height / 2, id("portraitOffsetX").value, id("portraitOffsetY").value);
+    drawImageOffsetScale(ctx, model, id("modelScale").value, modelPosX, modelPosY, id("modelOffsetX").value, id("modelOffsetY").value);
 
     ctx.drawImage(spark, 0, 0);
     ctx.drawImage(bar, 0, 0);
 
-    ctx.font = '60px dragalialost';
-    ctx.fillStyle = '#6a551f';
+    ctx.font = "60px dragalialost";
+    ctx.fillStyle = "#6a551f";
 
 
     let textWidth = ctx.measureText(adventurerName).width;
-    let rotateCenter = canvasWidth / 2;
+    let rotateCenter = canvas.width / 2;
     let padding = 10;
     let textStart = rotateCenter - (textWidth + elementImg.width + padding) / 2 + elementImg.width + padding;
 
@@ -61,7 +59,7 @@
 
     ctx.fillText(adventurerName, textStart + 5, textYPos + 5);
 
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = "white";
 
     ctx.fillText(adventurerName, textStart, textYPos);
 
@@ -71,7 +69,12 @@
     ctx.rotate(-textAngle * Math.PI / 180);
     ctx.translate(-rotateCenter, -textYPos);
 
-    id('download').href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    id("download").href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+
+    const previewCanvas = id("preview");
+    const previewCtx = previewCanvas.getContext("2d");
+    previewCtx.drawImage(canvas, 0, 0, previewCanvas.width, previewCanvas.height);
   }
 
   function drawImageOffsetScale(ctx, image, scale, centerX, centerY, offsetX, offsetY) {
@@ -93,7 +96,7 @@
 
   function loadImage(src){
     let img = new Image();
-    img.crossOrigin = 'anonymous';
+    img.crossOrigin = "anonymous";
     img.src = src;
     return new Promise((resolve, reject) => {
       img.onload = () => resolve(img);
